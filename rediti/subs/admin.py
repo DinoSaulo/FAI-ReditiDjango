8c9b253e0398b00c29707fd3ba4eb05e3dc25636
Saulo Alexandre de Barros <sab2@ug3c04.windows.cin.ufpe.br>
2019-01-15 09:30:26 -0300

    django admin.py do subs atualizado

--- rediti/subs/admin.py
+++ rediti/subs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Subrediti, Thread, Post
 
-# Register your models here.
+class SubreditiAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'creator']
+    search_fields = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Subrediti, SubreditiAdmin)
+admin.site.register(Thread)
+admin.site.register(Post)
+admin.site.register(Subscription)
